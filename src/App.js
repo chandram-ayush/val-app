731cae6003043c817ba7bd0bb0237f724d411d03
chandram-ayush <chandram.ayushwork@gmail.com>
2026-02-01 14:56:30 +0530

    bg_fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react';
 import './App.css';
-import bgImage from './bg_vd_1.jpg';
-import videoFile from './bg_video.mp4';
+import bgImage from './bg_val.jpg';
+import videoFile from './bg_vd_1.mp4';
 import musicFile from './song.mp3';
 
 function App() {
